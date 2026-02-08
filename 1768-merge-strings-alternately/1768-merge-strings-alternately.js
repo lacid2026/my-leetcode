951f1ee6fe0c6bba46e8686eddcf5387d8d50d3f
lacid2026 <lacid2026@gmail.com>
2026-02-08 17:49:06 +0900

    Time: 38 ms (87.68%), Space: 54.1 MB (51.2%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -7,18 +7,19 @@ var mergeAlternately = function(word1, word2) {
     // The word1 and word2 have been merged 
     let mergeStr = "";
     let minLength = Math.min(word1.length, word2.length);
-    console.log(minLength);
+    let maxLength = Math.max(word1.length, word2.length);
+    console.log(`maxLength::${maxLength}`);
     // 1. 길이가 같은 부분까지는 순서대로 붙임
-    for (let i=0; i< minLength; i++) {
-        mergeStr += word1.at(i);
-        mergeStr += word2.at(i);
+    for (let i=0; i< maxLength; i++) {
+        if (word1.at(i)) mergeStr += word1.at(i);
+        if (word2.at(i)) mergeStr += word2.at(i);
     }
-    console.log(`1.mergeStr::${mergeStr}`);
-    if (word1.length > minLength) {
-        mergeStr += word1.slice(minLength, word1.length);    
-    } else if (word2.length > minLength) {
-        mergeStr += word2.slice(minLength, word2.length);    
-    }
-    console.log(`2.mergeStr::${mergeStr}`);
+    // console.log(`1.mergeStr::${mergeStr}`);
+    // if (word1.length > minLength) {
+    //     mergeStr += word1.slice(minLength, word1.length);    
+    // } else if (word2.length > minLength) {
+    //     mergeStr += word2.slice(minLength, word2.length);    
+    // }
+    // console.log(`2.mergeStr::${mergeStr}`);
     return mergeStr;
 };

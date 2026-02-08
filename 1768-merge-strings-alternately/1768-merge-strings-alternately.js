bc45616b719a38fdc66ae106978a5029409acf61
lacid2026 <lacid2026@gmail.com>
2026-02-08 17:52:36 +0900

    Time: 44 ms (63.03%), Space: 55.6 MB (10.51%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -8,7 +8,7 @@ var mergeAlternately = function(word1, word2) {
     let mergeStr = "";
     let minLength = Math.min(word1.length, word2.length);
     let maxLength = Math.max(word1.length, word2.length);
-    console.log(`maxLength::${maxLength}`);
+    // console.log(`maxLength::${maxLength}`);
     // 1. 길이가 같은 부분까지는 순서대로 붙임
     for (let i=0; i< maxLength; i++) {
         if (word1.at(i)) mergeStr += word1.at(i);

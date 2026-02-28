5da01478015278a81cbd32e911da3b2be997e3e3
lacid2026 <lacid2026@gmail.com>
2026-02-28 23:06:47 +0900

    Time: 21 ms (5.31%), Space: 59.6 MB (78.1%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    /*
+        생각: 
+        - 순회하면서 0숫자를 카운트 한다
+        - 배열에서 0을 제거한다
+        - 배열의 맨 뒤에 0을 push한다.
+
+    */
+    let zero_cnt = 0;
+    let n = nums.length;
+    for (let i=0; i< n; i++) {
+        if (nums[i] === 0 ) {
+            zero_cnt++;  
+            nums.splice(i,1);
+            i--;          
+        }    
+    }  
+    console.log(`zero_cnt::${zero_cnt}`);
+    console.log('nums::step1', nums);
+    for (let i=0; i< zero_cnt; i++) {
+        nums.push(0);
+    }    
+    // console.log('nums::step2', nums);
+};

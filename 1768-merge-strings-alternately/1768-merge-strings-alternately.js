b9dcffc33efc5279aa8155e77b0128cc1f341e66
lacid2026 <lacid2026@gmail.com>
2026-07-25 16:28:02 +0900

    Time: 47 ms (44.32%), Space: 55.2 MB (10.17%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -4,15 +4,23 @@
  * @return {string}
  */
 var mergeAlternately = function(word1, word2) {
-    // The word1 and word2 have been merged 
-    let mergeStr = "";
+    let answer = "";
     let minLength = Math.min(word1.length, word2.length);
-    let maxLength = Math.max(word1.length, word2.length);
-    // console.log(`maxLength::${maxLength}`);
-    // 1. 길이가 같은 부분까지는 순서대로 붙임
-    for (let i=0; i< maxLength; i++) {
-        if (word1.at(i)) mergeStr += word1.at(i);
-        if (word2.at(i)) mergeStr += word2.at(i);
+    
+    for (let i =0; i<minLength; i++) {
+        answer += word1[i];
+        answer += word2[i];
     }
-    return mergeStr;
+    if (word1.length >minLength) {
+        let remainWord = word1.substring(minLength, word1.length);
+        // console.log(`remainWord1:${remainWord}`);
+        answer += word1.substring(minLength, word1.length);
+    }    
+    if (word2.length >minLength) {
+        let remainWord = word2.substring(minLength);
+        // console.log(`remainWord2:${remainWord}`);
+        answer += word2.substring(minLength, word2.length);
+    }    
+    // console.log(`answer:${answer}`);
+    return answer;
 };
